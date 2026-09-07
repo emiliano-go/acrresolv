be50b17217f5cce31c1d823ff3d9a0f23086a2ae
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-09-06 21:44:17 -0300

    chore: keep utils.py for backward compatibility

--- src/acrresolv/utils.py
+++ src/acrresolv/utils.py
@@ -1,24 +1,30 @@
 from cleantext import clean_words
 
+
 def clean_origin(origin: str, remove_stopwords: bool = True) -> list[str]:
     words = clean_words(
         origin,
         clean_all=False,
         numbers=False,
-        stp_lang="spanish",
-        stopwords=remove_stopwords
+        stp_lang="english",
+        stopwords=remove_stopwords,
     )
     return [w for w in words if w]
+
 
 def get_initials(words: list[str]) -> list[str]:
     return [w[0].upper() for w in words if w and w[0].isalpha()]
 
+
 def build_acro(initials: list[str]) -> str:
     return "".join(initials)
 
-def rule_based(text: str, remove_stopwords : bool) -> str:
-    words = clean_origin(text, remove_stopwords)
-    return build_acro(get_initials(words))
 
-def is_confident(acc: str) -> bool:
-    return 2 <= len(acc) <= 6
+def rule_based(text: str, remove_stopwords: bool) -> str:
+    words = clean_origin(text, remove_stopwords)
+    initials = get_initials(words)
+    return build_acro(initials)
+
+
+def is_confident(acro: str) -> bool:
+    return 2 <= len(acro) <= 6
